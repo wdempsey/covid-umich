3ede8aa0d25b8d3c391d0cff3ce5213d2bce35c6
Walter Dempsey <dempsey.walter@gmail.com>
2020-05-07 12:50:39 -0400

    SIR updates

--- methods/sir_example.R
+++ methods/sir_example.R
@@ -35,7 +35,7 @@ out1$time <- NULL
 out2$time <- NULL
 ## Show data
 png(filename = "C:/Users/wdem/Documents/GitHub/covid-umich/methods/figs/sir.png",
-    width = 480, height = 480, units = "px", pointsize = 12)
+    width = 6.5, height = 4, units = "in", res = 1200, pointsize = 12)
 par(mar = c(1.5,2,1,1)+0.1)
 plot(times, out1$I, type = "l", axes = FALSE, bty = "n")
 lines(times, out2$I, col = "red")
@@ -46,18 +46,20 @@ dev.off()
 
 ## Setup for bias plots
 f = 0.02
-M = 4.75
+M = 4
 days = 1:365
+FP = 0.01; FN = 0.10
 bary1 = -diff(out1$S)
 bary2 = -diff(out2$S)
 
-bias_fn <- function(M, bary, f, FP, FN) {
+bias_fn <- function(M, bary, f, FP = 0.01, FN = 0.15) {
   const = (bary*M + (1-bary))
   f0 = f/const
   f1 = M*f0
   Delta = f1 - f0
+  D_M = 1 + FP + FN - Delta * bary/(1-bary) * (FP*(1-bary) + FN * bary)/ (f0 * (1-bary) + f1 * bary)
   rho = Delta*sqrt(bary*(1-bary))/sqrt(f*(1-f))
-  error = rho*sqrt((1-f)/f) * sqrt(1-bary)/sqrt(bary)
+  error = rho*sqrt((1-f)/f) * sqrt(1-bary)/sqrt(bary)*D_M
   lag_error = error[1:(length(error)-1)]
   cur_error = error[2:(length(error))]
   
@@ -89,15 +91,51 @@ lines(bias2$r+bias2$bias, col = "red", lty = 2)
 
 ymax = max(bias1$Rt+bias1$logbias, bias1$Rt, bias2$Rt+bias2$logbias, bias2$Rt, na.rm = TRUE)
 ymin = min(bias1$Rt+bias1$logbias, bias1$Rt, bias2$Rt+bias2$logbias, bias2$Rt, na.rm = TRUE)
-png(filename = "C:/Users/wdem/Documents/GitHub/covid-umich/methods/figs/sir_rt.png",
-    width = 480, height = 480, units = "px", pointsize = 12)
-par(mar = c(1.5,3,1,1)+0.1)
-plot(bias1$Rt, type = "l", ylim = c(ymin, ymax), axes = FALSE, bty = "n")
+png(filename = "C:/Users/wdem/Documents/GitHub/covid-umich/methods/figs/sir_rt_comparison.png",
+    width = 6.5, height = 4, units = "in", res = 1200, pointsize = 12)
+par(mar = c(1.5,3,0,1)+0.1)
+plot(bias1$Rt, type = "l", ylim = c(0.7, 1.3), axes = FALSE, bty = "n")
 lines(bias1$Rt + bias1$logbias, lty = 2)
 abline(h = 1.0, col = "grey", lty = 2)
 lines(bias2$Rt, lty = 1, col = "red")
 lines(bias2$Rt+bias2$logbias, lty = 2, col = "red")
 axis(side =1, labels = FALSE)
+axis(side =2, cex.axis = 0.75, at = seq(0.8,1.2,0.1))
+mtext(expression(R[t]), side =2, line = 2, cex = 0.75)
+dev.off()
+
+## Same curve, just different M value
+M1 = 3; M2 = 4; M3 = 5
+bias1 = bias_fn(M1, bary2, f, FP, FN)
+bias2 = bias_fn(M2, bary2, f, FP, FN)
+bias3 = bias_fn(M3, bary2, f, FP, FN)
+
+ymax = max(bias3$r+bias3$bias)
+ymin = min(bias3$r+bias3$bias)
+png(filename = "C:/Users/wdem/Documents/GitHub/covid-umich/methods/figs/sir_ratio.png",
+    width = 6.5, height = 4, units = "in", res = 1200, pointsize = 12)
+plot(bias1$r, type = "l", ylim = c(ymin, ymax), axes = FALSE, bty = "n", xlab = "", ylab = "")
+lines(bias1$r+bias1$bias, lty = 2)
+lines(bias2$r+bias2$bias, col = "blue", lty = 2)
+lines(bias3$r+bias3$bias, col = "red", lty = 2)
+axis(side =1, labels = FALSE)
 axis(side =2, cex.axis = 0.75)
 mtext(expression(R[t]), side =2, line = 2, cex = 0.75)
+legend(35, 3.0, legend = c("M=1", "M=2", "M=3", "M=4"), lty = c(1,2,2,2), col = c("black", "black", "blue", "red"), cex = 0.75, bty = "n")
+dev.off()
+
+ymax = max(bias3$Rt+bias3$logbias)
+ymin = min(bias3$Rt+bias3$logbias)
+png(filename = "C:/Users/wdem/Documents/GitHub/covid-umich/methods/figs/sir_rt.png",
+    width = 6.5, height = 4, units = "in", res = 1200, pointsize = 12)
+par(mar = c(1.5,3,1,1)+0.1)
+plot(bias1$Rt, type = "l", ylim = c(ymin, ymax), axes = FALSE, bty = "n")
+lines(bias1$Rt + bias1$logbias, lty = 2)
+abline(h = 1.0, col = "grey", lty = 2)
+lines(bias2$Rt+bias2$logbias, lty = 2, col = "blue")
+lines(bias3$Rt+bias3$logbias, lty = 2, col = "red")
+axis(side =1, labels = FALSE)
+axis(side =2, cex.axis = 0.75)
+mtext(expression(R[t]), side =2, line = 2, cex = 0.75)
+legend(35, 1.175, legend = c("M=1", "M=2", "M=3", "M=4"), lty = c(1,2,2,2), col = c("black", "black", "blue", "red"), cex = 0.75, bty = "n")
 dev.off()
