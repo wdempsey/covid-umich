933eff7154760ee6b4f550b40eaa362371c972a9
Walter Dempsey <dempsey.walter@gmail.com>
2020-11-09 14:04:38 -0500

    Update IN_analysis_sample.R

--- methods/IN_analysis_sample.R
+++ methods/IN_analysis_sample.R
@@ -6,6 +6,8 @@ NR_rate[['sex']] = c(0.582, 0.418)
 NR_rate[['age']] = c(0.394, 0.411, 0.195)
 NR_rate[['race']] = c(0.231, 0.769)
 NR_rate[['fever']] = c(0.170, 0.830)
+NR_rate[['household']] = c(0.108, 0.892)
+NR_rate[['prior']] = c(0.061, 0.939)
 
 NR_test[['sex']] = c(0.217, 0.242)
 NR_test[['sex']] = NR_test[['sex']] * overall_test/sum(NR_rate[['sex']]*NR_test[['sex']])
@@ -15,28 +17,40 @@ NR_test[['race']] = c(0.195, 0.250)
 NR_test[['race']] = NR_test[['race']] * overall_test/sum(NR_rate[['race']]*NR_test[['race']])
 NR_test[['fever']] = c(0.664, 0.156)
 NR_test[['fever']] = NR_test[['fever']] * overall_test/sum(NR_rate[['fever']]*NR_test[['fever']])
+NR_test[['household']] = c(0.461, 0.216)
+NR_test[['household']] = NR_test[['household']] * overall_test/sum(NR_rate[['household']]*NR_test[['household']])
+NR_test[['prior']] = c(0.392, 0.216)
+NR_test[['prior']] = NR_test[['prior']] * overall_test/sum(NR_rate[['prior']]*NR_test[['prior']])
 
 R_rate[['sex']] = c(0.507, 0.493) # CENSUS
 R_rate[['age']] = c(0.527, 0.252, 0.220) # CENSUS
 R_rate[['race']] = c(0.869, 0.131) # CENSUS
 R_rate[['fever']] = c(0.018, 0.982) # RANDOM SAMPLE
+R_rate[['household']] = c(0.014, 0.986) # RANDOM SAMPLE
+R_rate[['prior']] = c(0.014, 0.986) # RANDOM SAMPLE
 
 R_test[['sex']] = c(0.014, 0.021) # RANDOM SAMPLE
 R_test[['age']] = c(0.017, 0.021, 0.009) # RANDOM SAMPLE
 R_test[['race']] = c(0.015, 0.034) # RANDOM SAMPLE
 R_test[['fever']] = c(0.045, 0.013) # RANDOM SAMPLE
+R_test[['household']] = c(0.294, 0.013) # RANDOM SAMPLE
+R_test[['prior']] = c(0.244, 0.013) # RANDOM SAMPLE
 
-weights = NR_array_rate = R_array_rate = array(0,dim = c(2,3,2,2))
+weights = NR_array_rate = R_array_rate = array(0,dim = c(2,3,2,2,2,2))
 
 for (i in 1:2) {
   for (j in 1:3) {
     for (k in 1:2) {
       for (l in 1:2) {
-        numer = R_rate[['sex']][i] * R_rate[['age']][j] * R_rate[['race']][k] * R_rate[['fever']][l] 
-        denom = NR_rate[['sex']][i] * NR_rate[['age']][j] * NR_rate[['race']][k] * NR_rate[['fever']][l] 
-        weights[i,j,k,l] = numer/denom
-        R_array_rate[i,j,k,l] = numer
-        NR_array_rate[i,j,k,l] = denom
+        for (m in 1:2) {
+          for (n in 1:2) {
+            numer = R_rate[['sex']][i] * R_rate[['age']][j] * R_rate[['race']][k] * R_rate[['fever']][l] * R_rate[['household']][m] * R_rate[['prior']][n]  
+            denom = NR_rate[['sex']][i] * NR_rate[['age']][j] * NR_rate[['race']][k] * NR_rate[['fever']][l] * NR_rate[['household']][m] * NR_rate[['prior']][n] 
+            weights[i,j,k,l,m,n] = numer/denom
+            R_array_rate[i,j,k,l,m,n] = numer
+            NR_array_rate[i,j,k,l,m,n] = denom
+          }
+        }
       }
     }
   }
@@ -45,29 +59,39 @@ for (i in 1:2) {
 weights = weights/sum(weights)
 
 ## GET THE ARRAY OF TEST RATES
-NR_array_test = array(0,dim=c(2,3,2,2))
-NR_array_test[,,,] = overall_test
+NR_array_test = array(0,dim=c(2,3,2,2,2,2))
+NR_array_test[,,,,,] = overall_test
 colnames = c('sex', 'age', 'race', 'fever')
 
 for(i in 1:2) {
-  hatmu = sum(NR_array_test[i,,,] * NR_array_rate[i,,,]/sum(NR_array_rate[i,,,]))
+  hatmu = sum(NR_array_test[i,,,,,] * NR_array_rate[i,,,,,]/sum(NR_array_rate[i,,,,,]))
   truemu = NR_test[['sex']][i]
-  NR_array_test[i,,,] = NR_array_test[i,,,] * truemu/hatmu
+  NR_array_test[i,,,,,] = NR_array_test[i,,,,,] * truemu/hatmu
 }
 for(j in 1:3) {
-  hatmu = sum(NR_array_test[,j,,] * NR_array_rate[,j,,]/sum(NR_array_rate[,j,,]))
+  hatmu = sum(NR_array_test[,j,,,,] * NR_array_rate[,j,,,,]/sum(NR_array_rate[,j,,,,]))
   truemu = NR_test[['age']][j]
-  NR_array_test[,j,,] = NR_array_test[,j,,] * truemu/hatmu
+  NR_array_test[,j,,,,] = NR_array_test[,j,,,,] * truemu/hatmu
 }
 for(k in 1:2) {
-  hatmu = sum(NR_array_test[,,k,] * NR_array_rate[,,k,]/sum(NR_array_rate[,,k,]))
+  hatmu = sum(NR_array_test[,,k,,,] * NR_array_rate[,,k,,,]/sum(NR_array_rate[,,k,,,]))
   truemu = NR_test[['race']][k]
-  NR_array_test[,,k,] = NR_array_test[,,k,] * truemu/hatmu
+  NR_array_test[,,k,,,] = NR_array_test[,,k,,,] * truemu/hatmu
 }
 for(l in 1:2) {
-  hatmu = sum(NR_array_test[,,,l] * NR_array_rate[,,,l]/sum(NR_array_rate[,,,l]))
+  hatmu = sum(NR_array_test[,,,l,,] * NR_array_rate[,,,l,,]/sum(NR_array_rate[,,,l,,]))
   truemu = NR_test[['fever']][l]
-  NR_array_test[,,,l] = NR_array_test[,,,l] * truemu/hatmu
+  NR_array_test[,,,l,,] = NR_array_test[,,,l,,] * truemu/hatmu
+}
+for(m in 1:2) {
+  hatmu = sum(NR_array_test[,,,,m,] * NR_array_rate[,,,,m,]/sum(NR_array_rate[,,,,m,]))
+  truemu = NR_test[['household']][m]
+  NR_array_test[,,,,m,] = NR_array_test[,,,,m,] * truemu/hatmu
+}
+for(n in 1:2) {
+  hatmu = sum(NR_array_test[,,,,,n] * NR_array_rate[,,,,,n]/sum(NR_array_rate[,,,,,n]))
+  truemu = NR_test[['prior']][n]
+  NR_array_test[,,,,,n] = NR_array_test[,,,,,n] * truemu/hatmu
 }
 
 sum(NR_array_test * weights)
