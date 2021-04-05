32a465d33a92b101db4f73cf7d8b733b4ec65319
Walter Dempsey <dempsey.walter@gmail.com>
2021-04-05 11:59:29 -0400

    fixing code to pull proper testing in waves 5/6/7

--- preprocessing/fb-fortestingonly-merge-code.R
+++ preprocessing/fb-fortestingonly-merge-code.R
@@ -6,9 +6,7 @@
 
 library("lubridate")
 
-fb_data = read.csv("../data/fb_indiana_data.csv")
-indiana_data = readRDS("../data/weeklycoviddata.RDS")
-
+fb_data = read.csv("../data/fb_test_data.csv")
 fb_data$day = day(fb_data$date)
 fb_data$week = week(fb_data$date) 
 weekweight = aggregate(day ~ week, fb_data, function(x){length(unique(x))})
@@ -16,37 +14,39 @@ weekweight = aggregate(day ~ week, fb_data, function(x){length(unique(x))})
 ## Build weekly version of FB data first
 
 weeksin2020 = 14:53
-all_data = data.frame(gender = rep(0,0), age = rep(0,0), fever = rep(0,0), 
+all_data_got = all_data_pos = data.frame(gender = rep(0,0), age = rep(0,0), fever = rep(0,0), 
                       weight_gottested = rep(0,0), weight = rep(0,0), week = rep(0,0),
                       year = rep(0,0))
 
+tested_fbdata = subset(fb_data, gottested == 1)
+rm(fb_data)
+
 for (week in weeksin2020) {
+  print(paste("At week", week, "in year 2020"))
   addweight = 7/weekweight$day[weekweight$week == week]
-  temp = fb_data[week(fb_data$date) == week & year(fb_data$date) == 2020,]
+  temp = tested_fbdata[week(tested_fbdata$date) == week & year(tested_fbdata$date) == 2020,]
   temp = subset(temp, is.element(gender, c(1,2)))
-  temp$weight_gottested = temp$weight * temp$gottested
-  temp$weight_postested = temp$weight * temp$postest
-  temp = subset(temp, !is.na(weight_gottested))
-  total_weight = sum(temp$weight)
-  total_weightgottested = sum(temp$weight_gottested)
-  agg_temp = aggregate(weight_gottested ~ gender+ age + fever, temp, sum)
-  agg_temp2 = aggregate(weight ~ gender+ age + fever, temp, sum)
-  agg_temp3 = aggregate(weight_postested ~ gender+ age + fever, temp, sum)
-  # total_weightpostested = sum(temp$weight_postested, na.rm = TRUE)
-  # agg_temp$posweight = NA
-  # for(row in 1:nrow(agg_temp)) {
-  #  which_row = which(agg_temp3$gender == agg_temp[row,1] & agg_temp3$age == agg_temp[row,2] & agg_temp3$fever == agg_temp[row,3])
-  #   if(length(which_row) > 0) {
-  #     agg_temp$posweight[row] = agg_temp3$weight_postested[row]
-  #   }
-  # }
+  temp$weightfever = temp$weight * temp$fever
+  temp = subset(temp, !is.na(weightfever))
+  total_weight = sum(temp$weight[temp$gottested ==1])
+  total_weightfever = sum(temp$weightfever[temp$gottested ==1])
+  agg_temp = aggregate(weightfever ~ gender+ age, subset(temp, gottested == 1), sum)
+  agg_temp2 = aggregate(weight ~ gender+ age, subset(temp, gottested == 1), sum)
   agg_temp$weight = agg_temp2$weight
-  agg_temp$weight_gottested = agg_temp$weight_gottested/sum(agg_temp$weight_gottested) * total_weightgottested * addweight
+  agg_temp$weightfever = agg_temp$weightfever/sum(agg_temp$weightfever) * total_weightfever * addweight
   agg_temp$weight = agg_temp$weight/sum(agg_temp$weight) * total_weight * addweight
-  # agg_temp$posweight = agg_temp$posweight/sum(agg_temp$posweight) * total_weightpostested * addweight
+  all_data_got = rbind(all_data_got, agg_temp)
+  
+  total_weight = sum(temp$weight[temp$postest ==1])
+  total_weightfever = sum(temp$weightfever[temp$postest ==1])
+  agg_temp = aggregate(weightfever ~ gender+ age, subset(temp, postest == 1), sum)
+  agg_temp2 = aggregate(weight ~ gender+ age, subset(temp, postest == 1), sum)
+  agg_temp$weight = agg_temp2$weight
+  agg_temp$weightfever = agg_temp$weightfever/sum(agg_temp$weightfever) * total_weightfever * addweight
+  agg_temp$weight = agg_temp$weight/sum(agg_temp$weight) * total_weight * addweight
   agg_temp$week = rep(week, nrow(agg_temp))
   agg_temp$year = rep(2020, nrow(agg_temp))
-  all_data = rbind(all_data, agg_temp)
+  all_data_pos = rbind(all_data_pos, agg_temp)
 }
 
 weeksin2021 = 1:5
---
+++ preprocessing/fb_full_fortestingonly_cleaning.R
@@ -0,0 +1,58 @@
+data_files = list.files(path = "D:/fb-data/all-fb-data/monthly_data/")
+
+file_name = "../data/fb_test_data.csv"
+
+library("lubridate")
+
+for(i in 1:length(data_files)) {
+
+  temp = read.csv(paste("D:/fb-data/all-fb-data/monthly_data/",data_files[i], sep=""))
+  
+  state_temp = temp #subset(temp,is.element(A3b, c(15,18,23,36))) # Bringing in Ohio/Michigan/Indiana/Kentucky for more data on this problem.
+  
+  time <- ymd_hms(state_temp$StartDatetime)
+  day_df = data.frame("date" = date(time), "weight" = state_temp$weight)
+  
+  fever <- function(x) {any(as.numeric(unlist(strsplit(x, split = ",")))==1)}
+  cough <- function(x) {any(as.numeric(unlist(strsplit(x, split = ",")))==2)}
+  shortness <- function(x) {any(as.numeric(unlist(strsplit(x, split = ",")))==3)}
+  diffbreath <- function(x) {any(as.numeric(unlist(strsplit(x, split = ",")))==4)}
+  
+  day_df$fever = unlist(lapply(state_temp$B2,fever))
+  day_df$cough = unlist(lapply(state_temp$B2,cough))
+  day_df$shortness = unlist(lapply(state_temp$B2,shortness))
+  day_df$diffbreath = unlist(lapply(state_temp$B2,diffbreath))
+  day_df$symptoms = (day_df$fever & day_df$cough) | day_df$shortness | day_df$diffbreath
+  
+  ### WAVE 3 Testing Info (only on symptomatic people)
+  wave4_included = any(is.element(state_temp$wave ,4))
+  if(!wave4_included) {
+    day_df$gottested = as.numeric(is.element(state_temp$B5, c(1,2,3)))
+    day_df$postest =  as.numeric(is.element(state_temp$B5, c(1)))
+    day_df$negtest =  as.numeric(is.element(state_temp$B5, c(2)))
+  } else { 
+    day_df$gottested = as.numeric(is.element(state_temp$B5, c(1,2,3)) * (state_temp$wave < 4) + (state_temp$B10 == 1)*(state_temp$wave >= 4))
+    day_df$postest =  as.numeric(is.element(state_temp$B5, c(1)) * (state_temp$wave < 4) + (state_temp$B10a == 1)*(state_temp$wave >= 4))
+    day_df$negtest =  as.numeric(is.element(state_temp$B5, c(2)) * (state_temp$wave < 4) + (state_temp$B10a == 1)*(state_temp$wave >= 4))
+  }
+  
+  day_df$gender = state_temp$D1 # 1 = MALE, 2 = Female, 3 = Non-binary, 4 = Self-Describe, 5 = Prefer not to answer
+  day_df$age = state_temp$D2 #  18-24 years (1), 25-34 years (2), 35-44 years (3), 45-54 years (4), 55-64 years (5), 65-74 years (6), 75 years or older (7)
+  if(!wave4_included) {
+    day_df$hospital = state_temp$B6
+  } else{
+    day_df$hospital = grepl("4", state_temp$B7) | grepl("5", state_temp$B7) | grepl("6", state_temp$B7)
+  }
+  
+  test_df = subset(day_df, gottested == 1)
+ 
+  day_df$weight_gottested = day_df$weight * day_df$gottested
+  day_df$weight_postested = day_df$weight * day_df$postest
+  
+  if(file.exists(file_name)){
+    write.table(file = file_name, x = day_df, append = TRUE, col.names = FALSE, row.names = FALSE, sep = ",")
+  } else{
+    write.table(file = file_name, x = day_df, row.names = FALSE, sep = ",")
+  }
+  
+}
