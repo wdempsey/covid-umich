88980e7a2c863cbe65357b44bcc8e342d29badbe
Walter Dempsey <dempsey.walter@gmail.com>
2021-04-06 11:55:31 -0400

    Update fb_cleaning.R
    Collecting correct testing for waves 5 and 6

--- preprocessing/fb_cleaning.R
+++ preprocessing/fb_cleaning.R
@@ -25,20 +25,20 @@ for(i in 1:length(data_files)) {
   day_df$symptoms = (day_df$fever & day_df$cough) | day_df$shortness | day_df$diffbreath
   
   ### WAVE 3 Testing Info (only on symptomatic people)
-  wave456_included = any(state_temp$wave >=4)
-  if(!wave4_included) {
+  wave456_included = any(indiana_temp$wave >=4)
+  if(!wave456_included) {
     day_df$gottested = as.numeric(is.element(indiana_temp$B5, c(1,2,3)))
     day_df$postest =  as.numeric(is.element(indiana_temp$B5, c(1)))
     day_df$negtest =  as.numeric(is.element(indiana_temp$B5, c(2)))
   } else { 
-    day_df$gottested = as.numeric(is.element(indiana_temp$B5, c(1,2,3)) * (indiana_temp$wave != 4) + (indiana_temp$B10 == 1)*(indiana_temp$wave == 4))
-    day_df$postest =  as.numeric(is.element(indiana_temp$B5, c(1)) * (indiana_temp$wave != 4) + (indiana_temp$B10a == 1)*(indiana_temp$wave == 4))
-    day_df$negtest =  as.numeric(is.element(indiana_temp$B5, c(2)) * (indiana_temp$wave != 4) + (indiana_temp$B10a == 1)*(indiana_temp$wave == 4))
+    day_df$gottested = as.numeric(is.element(indiana_temp$B5, c(1,2,3)) * (indiana_temp$wave < 4) + (indiana_temp$B10 == 1)*(indiana_temp$wave >= 4))
+    day_df$postest =  as.numeric(is.element(indiana_temp$B5, c(1)) * (indiana_temp$wave < 4) + (indiana_temp$B10a == 1)*(indiana_temp$wave >= 4))
+    day_df$negtest =  as.numeric(is.element(indiana_temp$B5, c(2)) * (indiana_temp$wave < 4) + (indiana_temp$B10a == 1)*(indiana_temp$wave >= 4))
   }
   
   day_df$gender = indiana_temp$D1 # 1 = MALE, 2 = Female, 3 = Non-binary, 4 = Self-Describe, 5 = Prefer not to answer
   day_df$age = indiana_temp$D2 #  18-24 years (1), 25-34 years (2), 35-44 years (3), 45-54 years (4), 55-64 years (5), 65-74 years (6), 75 years or older (7)
-  if(!wave4_included) {
+  if(!wave456_included) {
     day_df$hospital = indiana_temp$B6
   } else{
     day_df$hospital = grepl("4", indiana_temp$B7) | grepl("5", indiana_temp$B7) | grepl("6", indiana_temp$B7)
