c1fc53320fda48921d67ec5ec09b553cc064b7a9
Walter Dempsey <dempsey.walter@gmail.com>
2021-06-11 10:11:01 -0400

    Update 6_in_death_cleaning.R

--- preprocessing/6_in_death_cleaning.R
+++ preprocessing/6_in_death_cleaning.R
@@ -11,14 +11,14 @@ gender_data = read.csv("../data/covid_indiana_gender.csv")
 ethnicity_data = read.csv("../data/covid_indiana_ethnicity.csv")
 race_data = read.csv("../data/covid_indiana_race.csv")
 
-unique_dates = unique(age_data$..date)
+unique_dates = unique(age_data$DATE)
 
 library(lubridate)
 ## MAKE DATES
-age_data$date = mdy(age_data$ï..date)
-gender_data$date = mdy(gender_data$ï..date)
-ethnicity_data$date = mdy(ethnicity_data$ï..date)
-race_data$date = mdy(race_data$ï..date)
+age_data$date = ymd(age_data$DATE)
+gender_data$date = ymd(gender_data$DATE)
+ethnicity_data$date = ymd(ethnicity_data$DATE)
+race_data$date = ymd(race_data$DATE)
 
 age_data$week = week(age_data$date)
 gender_data$week = week(gender_data$date)
@@ -35,6 +35,11 @@ gender_data$day = day(gender_data$date)
 ethnicity_data$day = day(ethnicity_data$date)
 race_data$day = day(race_data$date)
 
+age_levels = levels(age_data$AGE_GRP)[-9]
+gender_levels = levels(gender_data$GENDER)[-3]
+ethnicity_levels = levels(ethnicity_data$ETHNICITY)[-3]
+race_levels = levels(race_data$RACE)[-4]
+
 ## Pull each week and year
 ## Build every combination
 ## WE USE: https://www.census.gov/prod/cen2010/briefs/c2010br-04.pdf
@@ -46,7 +51,7 @@ hist_match$census = census/sum(census)
 ## Not perfect but better than using independence which clearly does not hold.
 ## We assume independence for non-Hispanic.
 
-date = age_data$date[1]
+date = age_data$date[2000]
 
 construct_combos <- function(date) {
   subset_age = age_data[age_data$date == date,]
@@ -54,18 +59,15 @@ construct_combos <- function(date) {
   subset_ethnicity = ethnicity_data[ethnicity_data$date == date,]
   subset_race = race_data[race_data$date == date,]
   
-  allcombinations = expand.grid(startdate = min(subset_age$date), age = age_test$age, 
-                                gender = gender_test$GENDER, ethnicity = ethnicity_test$ETHNICITY, 
-                                race = race_test$RACE)
   ## FIX DEATHS
-  age_death = aggregate(COVID_DEATHS~age, data = subset_age, sum)
+  age_death = aggregate(COVID_DEATHS~AGE_GRP, data = subset_age, sum)
   gender_death = aggregate(COVID_DEATHS~GENDER, data = subset_gender, sum)
   ethnicity_death = aggregate(COVID_DEATHS~ETHNICITY, data = subset_ethnicity, sum)
   race_death = aggregate(COVID_DEATHS~RACE, data = subset_race, sum)
   total_deaths = sum(age_death$COVID_DEATHS)
   
   ## COMPUTE TESTING FRACTIONS IGNORING UNKNOWN
-  age_death$death_frac = age_death$COVID_DEATHS / sum(age_death$COVID_DEATHS[!age_death$age == 'Unknown'])
+  age_death$death_frac = age_death$COVID_DEATHS / sum(age_death$COVID_DEATHS[!age_death$AGE_GRP == 'Unknown'])
   gender_death$death_frac = gender_death$COVID_DEATHS / sum(gender_death$COVID_DEATHS[!gender_death$GENDER == 'Unknown'])
   ethnicity_death$death_frac = ethnicity_death$COVID_DEATHS / sum(ethnicity_death$COVID_DEATHS[!ethnicity_death$ETHNICITY == 'Unknown'])
   race_death$death_frac = race_death$COVID_DEATHS / sum(race_death$COVID_DEATHS[!race_death$RACE == 'Unknown'])
@@ -76,8 +78,8 @@ construct_combos <- function(date) {
   race_death$death_frac[is.nan(race_death$death_frac)] = 0
   
   ## REMOVE UNKNOWN
-  temp = (age_death$COVID_DEATHS[age_death$age == 'Unknown'] * age_death$death_frac[!age_death$age == 'Unknown'])
-  age_death = age_death[!age_death$age == 'Unknown',]
+  temp = (age_death$COVID_DEATHS[age_death$AGE_GRP == 'Unknown'] * age_death$death_frac[!age_death$AGE_GRP == 'Unknown'])
+  age_death = age_death[!age_death$AGE_GRP == 'Unknown',]
   age_death$COVID_DEATHS = age_death$COVID_DEATHS + temp
   
   temp = (gender_death$COVID_DEATHS[gender_death$GENDER == 'Unknown'] * gender_death$death_frac[!gender_death$GENDER == 'Unknown'])
@@ -103,17 +105,22 @@ construct_combos <- function(date) {
   ethnicity_death$death_frac[is.nan(ethnicity_death$death_frac)] = 0
   race_death$death_frac[is.nan(race_death$death_frac)] = 0
   
+  allcombinations = expand.grid(startdate = min(subset_age$date), age = age_levels, 
+                                gender = gender_levels, 
+                                ethnicity = ethnicity_levels, 
+                                race = race_levels)
+  
   allcombinations$covid_deaths = rep(NA, nrow(allcombinations))
   
   for(comb_iter in 1:nrow(allcombinations)) {
-    temp_age = age_death$death_frac[age_death$age == allcombinations[comb_iter,]$age]
-    temp_gender = gender_death$death_frac[gender_death$GENDER == allcombinations[comb_iter,]$gender]
-    temp_ethnicity = ethnicity_death$death_frac[ethnicity_death$ETHNICITY == allcombinations[comb_iter,]$ethnicity]
+    temp_age = age_death$death_frac[age_death$AGE_GRP == as.character(allcombinations[comb_iter,]$age)]
+    temp_gender = gender_death$death_frac[gender_death$GENDER == as.character(allcombinations[comb_iter,]$gender)]
+    temp_ethnicity = ethnicity_death$death_frac[ethnicity_death$ETHNICITY == as.character(allcombinations[comb_iter,]$ethnicity)]
     if(allcombinations[comb_iter,]$ethnicity == 'Hispanic or Latino') {
-      temp_race = hist_match$census[hist_match$race == allcombinations[comb_iter,]$race]
+      temp_race = hist_match$census[hist_match$race == as.character(allcombinations[comb_iter,]$race)]
       comb_frac = temp_age * temp_gender * temp_ethnicity * temp_race
     } else {
-      temp_race = race_death$death_frac[race_death$RACE == allcombinations[comb_iter,]$race]
+      temp_race = race_death$death_frac[race_death$RACE == as.character(allcombinations[comb_iter,]$race)]
       comb_frac = temp_age * temp_gender * temp_ethnicity * temp_race
     }
     allcombinations$covid_deaths[comb_iter] = total_deaths * comb_frac
