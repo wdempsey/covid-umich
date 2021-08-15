f890fa41dab95606cbf4723202a32fbb377dc94f
Walter Dempsey <dempsey.walter@gmail.com>
2021-08-15 14:20:15 -0400

    updaed paper through case study (all look good)

--- methods/11_effss_calcs.R
+++ methods/11_effss_calcs.R
@@ -14,7 +14,7 @@ N = 6.732e6
 f = sum(temp$COVID_TEST)/(N-sum(age_data$COVID_TEST[age_data$DATE < "2020-04-25"]))
 FP = 0.024
 FN = 0.13
-M = 1.3
+M = 1.5
 
 effss_calc <- function(prevalence, f, M, FN, FP) {
   ## Effective SS calculation
@@ -40,23 +40,25 @@ print(paste("Sampling rate:", round(f,4)))
 print(paste("False positive rate:", FP))
 print(paste("False negative rate:", FN))
 print(paste("Relative sampling rate:", M))
-print(paste("Effective sample size:", round(effss,0)))
+print(paste("Effective sample size:", round(effss1,0)))
 
 
 ## Even if 
 f2 = 0.01 # Increased sampling rate
-M2 = 1.1 # Decrease relative sampling rate
+M2 = 1.2 # Decrease relative sampling rate
 effss2 = effss_calc(prevalence, f2, M2, FN, FP) 
 
 print(paste("Sampling rate:", round(f2,4)))
 print(paste("Relative sampling rate:", M2))
-print(paste("Effective sample size:", round(effss_2,0)))
+print(paste("Effective sample size:", round(effss2,0)))
+print(paste("Factor increase:", round(effss2/effss1,1)))
 
 FN2 = 0.20 # Increase in the FN rate
 effss_newFN = effss_calc(prevalence, f2, M2, FN2, FP) 
 
 print(paste("Effective sample size:", round(effss_newFN,0)))
 print(paste("% Reduction in effective sample size:", round((effss2-effss_newFN)/effss2,3)))
+print(paste("Factor increase:", round(effss_newFN/effss1,1)))
 
 
 ## APPENDIX B.8 
