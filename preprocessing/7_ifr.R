e5257dc328f9e7867ea65343f2d2cc8619477044
Walter Dempsey <dempsey.walter@gmail.com>
2021-08-17 08:00:21 -0400

    ifr in writeup - need more model details

--- preprocessing/7_ifr.R
+++ preprocessing/7_ifr.R
@@ -3,8 +3,14 @@
 
 age = c(9, 33, 55,65,75,85)
 ifr = c(0.002, 0.05, 0.4,1.4,4.6,15)
-plot(age,log(ifr))
-abline(lm(log(ifr) ~ age))
+par(mar = c(4,4,0.1,1)+0.1)
+png(filename = "../figs/logifr.png",
+    width = 960, height = 480, units = "px", pointsize = 25)
+plot(age,log(ifr), bty = "n", xlab = "Age", ylab = "log(IFR)", xlim = c(0,80), ylim = c(-8,2))
+seq_age = seq(2,80,1)
+seq_logifr = predict(lm(log(ifr) ~ age), newdata=data.frame(age = seq_age))
+lines(x = seq_age, y = seq_logifr)
+dev.off()
 
 ## Suggests a linear fit on log-scale 
 fit <- lm(log(ifr) ~ age)
