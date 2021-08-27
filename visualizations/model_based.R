c503b9816cdc1362480eb42d936fe5780f430915
Walter Dempsey <dempsey.walter@gmail.com>
2021-08-27 10:14:01 -0400

    Update model_based.R

--- visualizations/model_based.R
+++ visualizations/model_based.R
@@ -65,3 +65,41 @@ ggplot(smr_pred, mapping = aes(x = date)) +
   scale_x_date(date_labels = "%b %Y")
 
 dev.off()
+
+## Build a plot for case count too
+
+summary_fit = summary(fit_forcing, pars = "pred_cases_per_agegroup", probs = c(0.025, 0.05, 0.1, 0.5, 0.9, 0.95, 0.975))$summary
+n_days = nrow(summary_fit)/6
+dates = c(min(dates)-1:(n_days - length(dates)), dates) ## EXTEND BACKWARDS FOR CASES
+timepoints = rep(dates, 6)
+age_group = rep(c(1:6), each = length(dates))
+summary_fit = data.frame(summary_fit)
+summary_fit$timepoints = timepoints
+summary_fit$age_group = age_group
+summary_fit$week = week(summary_fit$timepoints)
+summary_fit$year = year(summary_fit$timepoints)
+
+agg_cases = aggregate(mean ~ timepoints, data = summary_fit, FUN = sum)
+agg_cases$X5. = aggregate(X5. ~ timepoints, data = summary_fit, FUN = sum)$X5.
+agg_cases$X50. = aggregate(X50. ~ timepoints, data = summary_fit, FUN = sum)$X50.
+agg_cases$X95. = aggregate(X95. ~ timepoints, data = summary_fit, FUN = sum)$X95.
+
+png(filename = "../figs/cases_ppc.png",
+    width = 6.5, height = 4, units = "in", res = 1200, pointsize = 12)
+par(mar = c(2,2,1,1)+0.1)
+
+smr_pred <- cbind(agg_cases)
+smr_pred$date = date(dates)
+smr_pred = smr_pred[smr_pred$date < "2021-02-01",]
+c_posterior = my_palette # "orange"
+
+
+ggplot(smr_pred, mapping = aes(x = date)) +
+  #geom_ribbon(aes(ymin = X2.5., ymax = X97.5.), fill = c_dark, ) +
+  geom_ribbon(aes(ymin = X5., ymax = X95.), fill = c_posterior, alpha=0.35) +
+  #geom_ribbon(aes(ymin = X10., ymax = X90.), fill = c_light) +
+  geom_line(mapping = aes(x = date, y = X50.), color = c_posterior) +
+  labs(x = "Day", y = "Cases") +
+  scale_x_date(date_labels = "%b %Y")
+
+dev.off()
